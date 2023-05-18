57a6657226906573d0014eeaafdbdb71871e1cfc
Bruno Pequeno <bru.pequeno@gmail.com>
2023-05-17 21:12:54 -0300

    Initial commit

---
+++ src/SmartTV.java
@@ -0,0 +1,12 @@
+public class SmartTV {
+    boolean ligada = true;
+    int canal = 1;
+    int volume = 25;
+
+    public void ligar(){
+        ligada = true;
+    }
+    public void desligar(){
+        ligada = false;
+    }
+}
---
+++ src/Usuario.java
@@ -0,0 +1,11 @@
+public class Usuario {
+    public static void main(String[] args) {
+        SmartTV tv1 = new SmartTV();
+
+        tv1.ligar();
+        tv1.desligar();
+        System.out.println("Status da TV: " + tv1.ligada);
+        System.out.println("Canal: " + tv1.canal);
+        System.out.println("Volume: " + tv1.volume);
+    }
+}
